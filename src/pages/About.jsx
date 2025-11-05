9a5964825c028785da97b517618e90595feda129
McAyany <ayanyobala@gmail.com>
2025-11-05 11:39:31 +0300

    added page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,22 @@
 export default function About() {
-  return <h1 className="text-2xl font-semibold">About This App</h1>
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">About This App</h1>
+      <p>
+      This is a <b>React.js</b> application built using <b>Vite</b> and <b>Tailwind CSS</b>, covering the following major concepts:
+     
+         <ul> - Component architecture and reusability</ul>
+         <ul>- State management and hooks</ul>
+         <ul>- API integration</ul>
+         <ul>- Theming with Tailwind CSS (light/dark mode)</ul>
+         <ul>- Routing with React Router</ul>
+      
+      
+
+      The app includes a <b>Task Manager</b>, <b>API Integration Page</b>, and other reusable UI components like Navbar, Footer, Card, and Button.
+
+      </p>
+    </div>
+  );
+   
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,12 @@ import Card from "../components/Card";
 
 export default function Home() {
   return (
-    <div className="space-y-6">
-      <h1 className="text-3xl font-bold text-gray-800">Welcome to Home Page</h1>
-
+      
+    <div className="flex flex-col items-center justify-center text-center min-h-screen p-6 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-all">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My React App</h1>
+      <p className="text-lg max-w-xl mb-6">
+        This app demonstrates modern frontend practices — React, Tailwind, hooks, and API integration.
+      </p>
       <Card title="Actions">
         <div className="flex gap-3">
           <Button label="Primary" variant="primary" />
@@ -13,6 +16,12 @@ export default function Home() {
           <Button label="Danger" variant="danger" />
         </div>
       </Card>
+      <a
+        href="./About"
+        className="bg-blue-600 hover:bg-blue-700 text-white font-medium px-6 py-2 rounded-md transition-transform transform hover:scale-105"
+      >
+        Explore Features
+      </a>
     </div>
   );
 }
