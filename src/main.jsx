501fa298cf1033c7d4f23c8d5302ac31d6423673
McAyany <ayanyobala@gmail.com>
2025-11-05 17:01:13 +0300

    updated for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter} from 'react-router-dom' 
+import { BrowserRouter, HashRouter} from 'react-router-dom' 
 import ReactDOM from 'react-dom/client'
 import React from 'react' 
 import './index.css'
@@ -8,8 +8,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // ✅ Tailwind v4 plugin
+    tailwindcss(),
   ],
+  base: '/react-js-jsx-and-css-mastering-front-end-development-McAyany/', // ✅ important for GitHub Pages
 })
