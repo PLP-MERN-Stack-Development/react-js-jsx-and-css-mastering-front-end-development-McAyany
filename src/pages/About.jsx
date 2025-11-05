26a809c99510f51bf44a168a7311d3d1502dca70
McAyany <ayanyobala@gmail.com>
2025-11-05 11:54:34 +0300

    corrected semantic structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,23 @@
 export default function About() {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold">About This App</h1>
+    <div className="p-6 text-gray-800 dark:text-gray-100">
+      <h1 className="text-2xl font-semibold mb-4">About This App</h1>
+      <p className="mb-4">
+        This is a <b>React.js</b> application built using <b>Vite</b> and <b>Tailwind CSS</b>, covering the following major concepts:
+      </p>
+
+      <ul className="list-disc list-inside space-y-2 mb-4">
+        <li>Component architecture and reusability</li>
+        <li>State management and hooks</li>
+        <li>API integration</li>
+        <li>Theming with Tailwind CSS (light/dark mode)</li>
+        <li>Routing with React Router</li>
+      </ul>
+
       <p>
-      This is a <b>React.js</b> application built using <b>Vite</b> and <b>Tailwind CSS</b>, covering the following major concepts:
-     
-         <ul> - Component architecture and reusability</ul>
-         <ul>- State management and hooks</ul>
-         <ul>- API integration</ul>
-         <ul>- Theming with Tailwind CSS (light/dark mode)</ul>
-         <ul>- Routing with React Router</ul>
-      
-      
-
-      The app includes a <b>Task Manager</b>, <b>API Integration Page</b>, and other reusable UI components like Navbar, Footer, Card, and Button.
-
+        The app includes a <b>Task Manager</b>, <b>API Integration Page</b>, and other reusable UI components like 
+        <b> Navbar</b>, <b> Footer</b>, <b> Card</b>, and <b> Button</b>.
       </p>
     </div>
   );
-   
 }
