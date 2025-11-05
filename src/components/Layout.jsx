fc1fb6a303303d6938ef6719282971c053b27ef9
McAyany <ayanyobala@gmail.com>
2025-11-05 09:31:40 +0300

    added component architecture

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-grow container mx-auto p-6">{children}</main>
+      <Footer />
+    </div>
+  );
+}
